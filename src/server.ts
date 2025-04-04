7d0ade05b1f9509e784c51e561c6d3dadaf99f53
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-04-04 17:07:27 +0800

    feat(server): `npm start` for Toolforge

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import {createServer} from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const port = parseInt(process.env['PORT'] || '8000');
+
+createServer(({url}, res) => {
+	if (!url || url === '/') {
+		url = 'index.html'; // eslint-disable-line no-param-reassign
+	}
+	const file = new URL(`http://localhost/${path.join('reports', url)}`).pathname.slice(1),
+		ext = path.extname(file);
+	let contentType: string;
+	switch (ext) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		default:
+			contentType = 'text/html';
+	}
+	if (fs.existsSync(file)) {
+		res.writeHead(200, {
+			'content-type': contentType,
+			'x-content-type-options': 'nosniff',
+			'cache-control': `max-age=${60 * 60 * 24 * 15}, public`,
+		});
+		res.end(fs.readFileSync(file), 'utf8');
+	} else {
+		res.writeHead(301, {Location: '/'});
+		res.end();
+	}
+}).listen(port);
