f77de9d27028b88c2ed4c44c6acfc08fbf177f69
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-04-12 20:06:08 +0800

    fix(package.json): remove useless files

--- scan.sh
+++ scan.sh
@@ -8,7 +8,7 @@ fi
 site="${1}wiki" # example: zh-yuewiki
 target="${1//-/_}wiki" # example: zh_yuewiki
 file="$target-latest-pages-articles.xml.bz2"
-if (( $# < 3 ))
+if (( $# < 4 ))
 then
 	curl --output-dir "$2" -O "https://dumps.wikimedia.org/$target/latest/$file"
 	npx getParserConfig "$site" "https://$1.wikipedia.org/w/"
