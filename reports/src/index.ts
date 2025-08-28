41d821b76be68b17d0ae5324a8e53bae14a2817d
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-08-28 14:03:31 +0800

    feat(index.html): document API endpoints

--- reports/src/index.ts
+++ reports/src/index.ts
@@ -2,6 +2,10 @@ import {load, addLink} from './common';
 
 declare const data: string[];
 
+if (location.hostname !== 'lint-wiki-dumps.toolforge.org') {
+	document.getElementById('api')!.style.display = 'none';
+}
+
 load('./data/index.js', () => {
 	document.getElementById('container')!.append(...data.map(
 		lang => addLink('div', `${lang}wiki`, `./wiki.html?lang=${lang}`),
